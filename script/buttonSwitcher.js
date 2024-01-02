1d102d7315f6e9bc51a63903018eefbdd5799cb2
DevSaLLein <isaac.andrade3@aluno.ce.gov.br>
2024-01-02 20:24:06 -0300

    Feat: Add Projects container

--- script/buttonSwitcher.js
+++ script/buttonSwitcher.js
@@ -4,10 +4,3 @@ const aboutMe__content = document.querySelector('.aboutMe__content');
 const handleClickButton = () => {
     alert('essa funcionalidade ainda não está pronta, estou trabalhando para que esteja pronta o quanto antes!')
 };
-
-const handleScaleContent = () => {
-    aboutMe__content.classList.toggle('scaleContainer');
-}
-
-button.addEventListener('click', handleClickButton);
-aboutMe__content.addEventListener('click', handleScaleContent);
